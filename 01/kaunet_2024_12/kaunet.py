10c17540c7e5420152c5d3e3060164f917e0bc86
sam8helloworld <samcreate0830@gmail.com>
2025-01-07 15:06:19 +0900

    feat: kaunet base info

---
+++ 01/kaunet_2024_12/kaunet.py
@@ -0,0 +1,16 @@
+SITE_BASE_URL = "https://www.kaunet.com/"
+
+CARDBOARD = {
+    "name": "ダンボール",
+    "url": "https://www.kaunet.com/rakuraku/ranking/023/023002/",
+}
+
+CUSHIONING_PACKAGE = {
+    "name": "緩衝材",
+    "url": "https://www.kaunet.com/rakuraku/ranking/023/023003/0230030022/",
+}
+
+BAKING_PAPER = {
+    "name": "ベーキングペーパー",
+    "url": "https://www.kaunet.com/rakuraku/ranking/004/004006/0040060014/",
+}
